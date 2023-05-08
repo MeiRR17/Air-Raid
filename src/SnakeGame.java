3b63e501bafb4f43abd2bda17492e10094d0fef6
MeiRR17 <m.b.z.dvir@gmail.com>
2023-05-08 20:50:56 +0300

    something

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -3,6 +3,9 @@ import javax.swing.*;
 
 public class SnakeGame extends JPanel {
     public static void main(String[] args) {
+        System.out.println("somthng is wrong with ths thing" +
+                "is this the" +
+                "ss");
         System.out.println();
     }
 }

573270b623e97eef5b7f63a3002c9edd5dcd80fa
asafs <asafdf723@gmail.com>
2023-05-09 15:42:20 +0300

    something

---
+++ src/player1.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+    public class player1 {
+    public class Player {
+        private int x;
+        private int y;
+        public static final int SIZE =50 ;
+
+
+        public Player (int x, int y) {
+            this.x = x;
+            this.y = y;
+
+        }
+}

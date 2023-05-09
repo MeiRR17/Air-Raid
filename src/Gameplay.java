9350385f86ebceb44e4db0b7ba68a129e2ce8a5a
MeiRR17 <m.b.z.dvir@gmail.com>
2023-05-09 14:53:20 +0300

    something

---
+++ src/Gameplay.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+import java.awt.event.KeyListener;
+
+class Gameplay extends JPanel implements KeyListener {//
+
+
+}
---
+++ src/Window.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Window extends JFrame {
+
+    public Window(){
+        this.setResizable(false);
+        this.setSize(300,300);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.setBackground(Color.BLUE);
+
+    }
+    public void showWindow(){
+        this.setVisible(true);
+    }
+}

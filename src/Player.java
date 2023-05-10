2706605854e8194aad82a1e05cf53e58f7486cca
MeiRR17 <m.b.z.dvir@gmail.com>
2023-05-10 09:45:49 +0300

    something

---
+++ src/Player.java
@@ -0,0 +1,13 @@
+
+
+    public class Player {
+        private static final int PLANE_HEIGHT = 500;
+        private static final int PLANE_WIDTH = 500;
+    /*ADDED PLANE'S HEIGHT AND WIDTH*/
+        public static final int SIZE = 50;
+
+
+        public Player (int x, int y) {
+
+        }
+}

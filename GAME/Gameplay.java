df554dab46336341089a911ef33e2334ec65b49b
artem <artemriftin@gmail.com>
2023-05-11 11:39:56 +0300

    New

--- GAME/Gameplay.java
+++ GAME/Gameplay.java
@@ -8,12 +8,19 @@ import java.awt.event.KeyListener;
 
 class Gameplay extends JPanel implements KeyListener {
     /*In this class we're going to combine the whole game*/
+    private Player player;
+
 
     public Gameplay(int positionA, int positionB, int width, int height){
         this.setBounds(positionA, positionB, width, height);
-
         this.setBackground(Color.blue);
+        this.player = new Player(40,80);
     }
+    public void paintComponent(Graphics graphics){
+        super.paintComponent(graphics);
+        this.player.paint(graphics);
+    }
+
 
     @Override
     public void keyTyped(KeyEvent e) {
--- GAME/Player.java
+++ GAME/Player.java
@@ -1,14 +1,20 @@
+import java.awt.*;
 
-
-    public class Player {
+public class Player {
         /*In this class we're going to make player1's plane*/
         private static final int PLANE_HEIGHT = 500;
         private static final int PLANE_WIDTH = 500;
     /*ADDED PLANE'S HEIGHT AND WIDTH*/
-        private int x; //To get his place
-        private int y;
+        private int positionX; //To get his place
+        private int positionY;
 
         public Player (int x, int y) {
+            this.positionX =x;
+            this.positionY=y;
+        }
+        public static void paint(Graphics graphics){
+            graphics.fillRect(30,30,60,60);
+            graphics.setColor(Color.RED);
 
         }
 }

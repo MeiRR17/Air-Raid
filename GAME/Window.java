aca9f6fb034276df6193cbb77db31497fc9f27f3
MeiRR17 <m.b.z.dvir@gmail.com>
2023-05-11 18:58:51 +0300

    UPDATE Working on Window.java's Class

--- GAME/Window.java
+++ GAME/Window.java
@@ -5,10 +5,31 @@ import java.util.Objects;
 public class Window extends JFrame {
     private static final int WINDOW_WIDTH = 800;
     private static final int WINDOW_HEIGHT = 600;
-    ImageIcon icon = new ImageIcon("C:\\Users\\mbzdv\\IdeaProject\\A Game\\Resource\\gameIcon.jpgw");
+    ImageIcon icon = new ImageIcon("Resource/gameIcon.jpg");
 
 
     public Window(){
+
+        JPanel titleNamePanel = new JPanel();
+        titleNamePanel.setBounds(WINDOW_WIDTH/8, WINDOW_HEIGHT/8, WINDOW_WIDTH/4*3, WINDOW_HEIGHT/4);
+        titleNamePanel.setBackground(Color.BLUE.brighter());
+        JLabel titleNameLabel = new JLabel("Air Raid");
+        titleNameLabel.setForeground(Color.WHITE);
+        titleNameLabel.setBounds(WINDOW_WIDTH/8, WINDOW_HEIGHT / 8, WINDOW_WIDTH/4*3, WINDOW_HEIGHT/16*3);
+        titleNameLabel.setFont(new Font("Monospaced", Font.ITALIC, 30));
+
+        JPanel startButtonPanel = new JPanel();
+        startButtonPanel.setBounds(WINDOW_WIDTH / 8 * 3, WINDOW_HEIGHT/2, WIDTH / 4, HEIGHT / 8);
+        startButtonPanel.setBackground(Color.BLUE.brighter());
+
+        JButton startButton = new JButton("START");
+        startButton.setBackground(Color.BLUE.brighter());
+        startButton.setForeground(Color.WHITE);
+        startButton.setFont(new Font("Monospaced", Font.ITALIC, 30));
+
+        this.add(titleNamePanel);
+        this.add(startButtonPanel);
+
         setIconImage(icon.getImage());
         this.setTitle("Air Raid");
         this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);/*Set the size of the JFrame*/
@@ -20,10 +41,17 @@ public class Window extends JFrame {
         /*Create new Gameplay Object and make it the same size as the window*/
         this.add(gameplay);/*Add the gameplay object*/
 
-        JPanel titleNamePanel = new JPanel();
-        titleNamePanel.setBounds(WINDOW_WIDTH/8, WINDOW_HEIGHT/8, WINDOW_WIDTH/4*3, WINDOW_HEIGHT/4);
-        titleNamePanel.setBackground(Color.WHITE);
-        this.add(titleNamePanel);
+
+
+        JButton button = new JButton("button");
+        this.add(button);
+        button.setBounds(400,300,100,100);
+        button.addActionListener((e -> {
+            button.setBounds(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
+            button.setVisible(false);
+            this.add(gameplay);/*Add the gameplay object*/
+        }));
+        //we need to add a new button on the scene (Label), that is why I think we need a new class here...The
     }
     public void showWindow(){//This method will make the JFrame visible
         this.setVisible(true);/*JFrame's build-in method to set it to be visible*/
